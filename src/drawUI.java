ab5836b1fa1d3d25eef7567674097c0cecb7d63f
77Adson <11maczupaczu@gmail.com>
2024-12-29 00:06:53 +0100

    Working text version

--- src/drawUI.java
+++ src/drawUI.java
@@ -2,20 +2,21 @@
 import java.util.*;
 
 public class drawUI {
-    public static void showChampionSelect(List<String> availableChampions, List<String> team, String user) {
-        System.out.println(user + "'s Turn");
+    public static void showChampionSelect(List<String> availableChampions, List<String> team, List<String> enemyTeam) {
         System.out.println("Available Champions: " + availableChampions);
-        System.out.println(user + "'s Team: " + team);
+        System.out.println("Your team: " + team  + "\t" + enemyTeam + " :Enemy Team");
     }
 
+
+
+    @SuppressWarnings("resource")
     public static String getChampionChoice(List<String> availableChampions) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            String choice;
-            do {
-                System.out.print("Choose a champion: ");
-                choice = scanner.nextLine();
-            } while (!availableChampions.contains(choice));
-            return choice;
-        }
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+        do {
+            System.out.print("Choose a champion: ");
+            choice = scanner.nextLine();
+        } while (!availableChampions.contains(choice));
+        return choice;
     }
 }
--- src/main.java
+++ src/main.java
@@ -5,35 +5,68 @@ import java.util.*;
 
 
 public class main {
-    private static final List<String> champions = Arrays.asList("Champion1", "Champion2", "Champion3", "Champion4", "Champion5");
+    private static final List<String> champions = Arrays.asList("Ch1", "Ch2", "Ch3", "Ch4", "Ch5", "Ch6", "Ch7", "Ch8", "Ch9", "Ch10");
 
+    /**
+     * Starts the champion select process for a game of League of Legends.
+     *
+     * This method takes a socket and a boolean as parameters. The socket is
+     * used to communicate with the other player, and the boolean is used to
+     * determine whether the player is the server or the client.
+     *
+     * The champion select process is as follows:
+     *  1. The server and client each select a champion from the list of
+     *     available champions.
+     *  2. The server and client each send their chosen champion to the
+     *     other player.
+     *  3. The server and client each add the received champion to their
+     *     respective teams.
+     *  4. Steps 1-3 are repeated until each team has 4 champions.
+     *  5. The teams are saved to a file.
+     *
+     * @param socket the socket used to communicate with the other player
+     * @param turn a boolean indicating whether the player is the server
+     *                or the client
+     */
     public static void startChampionSelect(Socket socket, boolean isServer) {
         try (
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
         ) {
-            List<String> serverTeam = new ArrayList<>();
-            List<String> clientTeam = new ArrayList<>();
+            List<String> myTeam = new ArrayList<>();
+            List<String> enemyTeam = new ArrayList<>();
             List<String> availableChampions = new ArrayList<>(champions);
+            
+            boolean turn = isServer;
 
-            while (serverTeam.size() + clientTeam.size() < 4) {
-                if (isServer) {
-                    drawUI.showChampionSelect(availableChampions, serverTeam, "Server");
+
+            while (myTeam.size() < 4 || enemyTeam.size() < 4) {
+                System.out.println(isServer ? "I'm server" : "I'm client");
+                drawUI.showChampionSelect(availableChampions, myTeam, enemyTeam);
+
+                // Selecting turn
+                if (turn) {
                     String choice = drawUI.getChampionChoice(availableChampions);
-                    serverTeam.add(choice);
+                    myTeam.add(choice);
                     availableChampions.remove(choice);
                     out.println(choice);
+
+                // waiting turn
                 } else {
-                    drawUI.showChampionSelect(availableChampions, clientTeam, "Client");
-                    String choice = drawUI.getChampionChoice(availableChampions);
-                    clientTeam.add(choice);
-                    availableChampions.remove(choice);
-                    out.println(choice);
+                    String response = in.readLine();
+                    if (response == null) {
+                        System.out.println("Server sent null champion");
+                        // handle this error case
+                    } else {
+                        enemyTeam.add(response);
+                        availableChampions.remove(response);
+                        System.out.println("Received champion from server: " + response);
+                    }
                 }
-                isServer = !isServer;
+                turn = !turn;
             }
 
-            saveTeamsToFile(serverTeam, clientTeam);
+            saveTeamsToFile(myTeam, enemyTeam);
             System.out.println("Teams saved to file.");
         } catch (IOException e) {
             e.printStackTrace();
